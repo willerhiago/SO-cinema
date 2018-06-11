86d14bb63adca57a6ed38525b8ae0d31af248e96
gmfonseca <32571975+gmfonseca@users.noreply.github.com>
2018-06-11 09:01:01 -0300

    Melhoria na reserva de poltronas

--- SO-cinema/src/cinema/Sala.java
+++ SO-cinema/src/cinema/Sala.java
@@ -5,58 +5,65 @@ import java.io.IOException;
 import arquivo.Arquivo;
 
 public class Sala {
-	
+
 	int qntFilas;
 	int cadeirasPorFila;
 	String[][] poltronas;
-	
-	
+
+
 	public Sala() throws IOException {
 		Arquivo arq = new Arquivo();
 		this.poltronas = arq.lerSala();;
 		this.qntFilas = arq.getQntFilas();
 		this.cadeirasPorFila = arq.getQntCadeirasPorFila();
 	}
-	
-	
+
+
 	public boolean consultaAssento(int fileira, int cadeira) {
-		boolean result = false;
-		if(this.poltronas[fileira][cadeira].equals("1")) result = true;
-		return result;
-		
+		return true;
+
 	}
-	
-	public boolean reservaAssento(int fileira, int cadeira){
+
+	public synchronized boolean reservaAssento(int fileira, int cadeira){
 		boolean result = false;
-		if(consultaAssento(fileira,cadeira)) {
+		if(this.poltronas[fileira][cadeira].equals("1")) {
 			this.poltronas[fileira][cadeira] = "X";
-			result = true;
+			result=true;
 		}
 		return result;
 	}
-	
-	public boolean retiraReserva(int fileira, int cadeira) {
+
+	public synchronized boolean compraAssento(int fileira, int cadeira){
 		boolean result = false;
-		if(!consultaAssento(fileira,cadeira)) {
+		if(reservaAssento(fileira, cadeira)){
+			result = true;
+		}
+
+		return result;
+	}
+
+	public synchronized boolean retiraReserva(int fileira, int cadeira) {
+		boolean result = false;
+		if(this.poltronas[fileira][cadeira].equals("X")) {
 			this.poltronas[fileira][cadeira] = "1";
 			result = true;
 		}
 		return result;
 	}
-	
-	
+
+
 	public void escreverSala() {
-        if(qntFilas == 0) System.out.println("Nao existe sala!");
-        else {
-        	
-                for(String[] assentos2 : poltronas){
-                    for(String assento : assentos2){
-                        System.out.print(assento + "");
-                    }
-                    System.out.println();
-                }
-        }
-    }
+		if(qntFilas == 0) System.out.println("Nao existe sala!");
+		else {
+
+			for(String[] assentos2 : poltronas){
+				for(String assento : assentos2){
+					System.out.print(assento + "");
+				}
+				System.out.println();
+			}
+		}
+	}
 
 	//------------------------Getters 
 	public int getQntFilas() {
@@ -72,5 +79,5 @@ public class Sala {
 	public String[][] getPoltronas() {
 		return poltronas;
 	}
-	
+
 }
